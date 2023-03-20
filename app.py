e550bad93383dc6fe7a80d2fd5db16b4a6279112
Ziyi Chen <chenziyi1997@live.cn>
2023-03-20 16:48:08 -0700

    first plot

--- app.py
+++ app.py
@@ -11,19 +11,12 @@ import matplotlib.pyplot as plt
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
 # Load data (importing csv into pandas)
-
-df_original = pd.read_csv('van_houses-2022&2023.csv')
-
-# Take a random sample of 100 rows
-df = df_original.sample(frac=0.1, random_state=42)
-
-# Save the sampled dataframe as a CSV file
-df.to_csv('df_sampled.csv', index=False)
+df_original = pd.read_csv('df_sampled.csv')
 
 # dropdown selections
-zoning_classification = np.sort(df["zoning_classification"].unique())
-geo_local_area = np.sort(df["Geo Local Area"].unique())
-year = np.sort(df["report_year"].unique())
+zoning_classification = np.sort(df_original["zoning_classification"].unique())
+geo_local_area_list = np.sort(df_original["Geo Local Area"].unique())
+year = np.sort(df_original["report_year"].unique())
 
 #App layout
 app.layout = dbc.Container([
@@ -49,17 +42,17 @@ app.layout = dbc.Container([
     #2nd Dropdown selection for "geo_local_area"
     html.H6("Neigborhood"),
     dcc.Dropdown(id = "geo_local_area",
-                 value = 2022,
+                 value = ["Fairview","Downtown"],
                  #clearable = False, 
                  multi =True,
-                 options=[{'label': x, 'value': x} for x in geo_local_area],
+                 options=[{'label': x, 'value': x} for x in geo_local_area_list],
                  style = {'width': "50%"}),
     html.Br(),
 
     #3rd Dropdown selection for "zoning_classification"
     html.H6("Type of the Property"),
     dcc.Dropdown(id = "zoning_classification",
-                 value = 2022,
+                 value = ["Duplex","Industrial"],
                  #clearable = False, 
                  multi =True,
                  options=[{'label': x, 'value': x} for x in zoning_classification],
@@ -81,29 +74,29 @@ dcc.Graph(id="his_housing_price")
 @app.callback(
     Output("his_housing_price", "figure"),
     Input("report_year", "value"),
-     Input("geo_local_area", "value"),
-     Input("zoning_classification", "value")
-     
+    Input("geo_local_area", "value"),
+    Input("zoning_classification", "value")
 )
-
 def housing_price_histogram(year_slcted, neighborhood_slcted, type_property_slcted):
     
     #filter data based on user input
-    dff = df.copy()
-    dff = dff[dff['report_year'] == year_slcted]
-    dff = dff[(dff['geo_local_area'] == neighborhood_slcted ) & (dff['zoning_classification'] == type_property_slcted)]
+    df = df_original.copy()
+    df = df[df['report_year'] == year_slcted]
+    df = df[df["Geo Local Area"].isin(neighborhood_slcted)]
+    df = df[df["zoning_classification"].isin(type_property_slcted)]
+    # df = df[(df['Geo Local Area'] == neighborhood_slcted ) & (df['zoning_classification'] == type_property_slcted)]
     #dff = dff[dff['zoning_classification'] == type_property_slcted]
     
-
     #plot distribution of housing prices
     his = px.histogram(
-        dff, nbins=40,
+        df, nbins=40,
         x="current_land_value")
     his.update_traces(marker=dict(line=dict(width=1, color="white")))
     his.update_layout(
     xaxis_title_text='House Prices ($)', # xaxis label
     yaxis_title_text='Number of Houses', # yaxis label
     bargap=0.1)
+    
     return his
 
    
