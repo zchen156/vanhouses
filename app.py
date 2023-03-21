23f5f84b42eae1afe212bd1da99c5d6efc4ea778
Ziyi Chen <chenziyi1997@live.cn>
2023-03-20 19:48:49 -0700

    refine layout

--- app.py
+++ app.py
@@ -6,11 +6,18 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 import numpy as np
 import matplotlib.pyplot as plt
+import base64
+from PIL import Image
 
             
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY])
 
-# Load data (importing csv into pandas)
+# add pic
+image_path = 'dash_vanhouses.jpg'
+pil_img = Image.open("dash_vanhouses.jpg")
+
+
+# load data (importing csv into pandas)
 df_original = pd.read_csv('df_sampled.csv')
 
 # dropdown selections
@@ -18,68 +25,99 @@ zoning_classification = np.sort(df_original["zoning_classification"].unique())
 geo_local_area_list = np.sort(df_original["Geo Local Area"].unique())
 year = np.sort(df_original["report_year"].unique())
 
+#cards
+card_main = dbc.Card(
+    [
+        dbc.CardImg(src=pil_img, top=True, bottom=False, style={'height':'60%'}),
+        dbc.CardBody(
+            [
+                html.H1("Vancouver Housing Market", className="card-title"),
+                #html.H6("Lesson 1:", className="card-subtitle"),
+                html.P(
+                    "Our Python-based dashboard application gives an easy way for people to explore housing prices in Vancouver city.",
+                    className="card-text",
+                ),
+                #dcc.Dropdown(id='user_choice', options=[{'label': yr, "value": yr} for yr in df.year.unique()],
+                             #value=2007, clearable=False, style={"color": "#000000"}),
+                # dbc.Button("Press me", color="primary"),
+                # dbc.CardLink("GirlsWhoCode", href="https://girlswhocode.com/", target="_blank"),
+            ]
+        ),
+    ],
+    color="light",   # https://bootswatch.com/default/ for more card colors
+    style={"height": 350},
+    #inverse=True,   # change color of text (black or white)
+    outline=False,  # True = remove the block colors from the background and header
+)
+
+card_dd = dbc.Card(
+    dbc.CardBody(
+            [#1st Dropdown selection for "report_year"
+                html.H6("Year Reported"),
+                dcc.Dropdown(id = "report_year",
+                            value = 2022,
+                            clearable = False, 
+                            multi = False,
+                            options=[{'label': x, 'value': x} for x in year],
+                            style = {'width': "35%"}),
+                html.Br(),
+                html.Br(),
+
+                #2nd Dropdown selection for "geo_local_area"
+                html.H6("Neigborhood"),
+                dcc.Dropdown(id = "geo_local_area",
+                            value = ["Fairview","Downtown"],
+                            #clearable = False, 
+                            multi =True,
+                            options=[{'label': x, 'value': x} for x in geo_local_area_list],
+                            style = {'width': "100%"}),
+                html.Br(),
+                html.Br(),
+
+                #3rd Dropdown selection for "zoning_classification"
+                html.H6("Type of the Property"),
+                dcc.Dropdown(id = "zoning_classification",
+                            value = ["Commercial","Historical Area"],
+                            #clearable = False, 
+                            multi =True,
+                            options=[{'label': x, 'value': x} for x in zoning_classification],
+                            style = {'width': "100%"}),
+                
+            ]),
+            style={"height": 350}
+)
+
 #App layout
-app.layout = dbc.Container([
+app.layout = html.Div([  
+
+     dbc.Row([dbc.Col(card_main, width =5),
+             dbc.Col(card_dd, width =5)], justify="around"),
+
     
-    #App title
-    html.H1("Vancouver Housing Market", style = {'text-align': 'left'}),
     html.Br(),
-    
-    #Dropdown layout
+    html.Br(),
+
+    dbc.Row([
+    html.H5('Distribution of Housing Prices'),
+            dcc.Graph(id="his_housing_price")]),
+
     dbc.Row([
         dbc.Col([
+            html.H5('Bubble Chart of Housing Prices by Legal Type'),
+            dcc.Graph(id="bc_housing_price")
+        ]),
+
+        dbc.Col([
+            html.H5('Total Amount of houses in Different Neighborhood'),
+            dcc.Graph(id="pc_housing_price")
     
-    #1st Dropdown selection for "report_year"
-    html.H6("Year Reported"),
-    dcc.Dropdown(id = "report_year",
-                 value = 2022,
-                 clearable = False, 
-                 multi = False,
-                 options=[{'label': x, 'value': x} for x in year],
-                 style = {'width': "35%"}),
-    html.Br(),
-
-    #2nd Dropdown selection for "geo_local_area"
-    html.H6("Neigborhood"),
-    dcc.Dropdown(id = "geo_local_area",
-                 value = ["Fairview","Downtown"],
-                 #clearable = False, 
-                 multi =True,
-                 options=[{'label': x, 'value': x} for x in geo_local_area_list],
-                 style = {'width': "50%"}),
-    html.Br(),
-
-    #3rd Dropdown selection for "zoning_classification"
-    html.H6("Type of the Property"),
-    dcc.Dropdown(id = "zoning_classification",
-                 value = ["Commercial","Historical Area"],
-                 #clearable = False, 
-                 multi =True,
-                 options=[{'label': x, 'value': x} for x in zoning_classification],
-                 style = {'width': "50%"}),
-    
-                 ])
-    
-]),
+        ])
+        
+    ])
+]
+    ,style={'padding': 40})
 
 
-    
-html.Br(),
-html.H5('Distribution of Housing Prices'),
-dcc.Graph(id="his_housing_price"),
-html.Br(),
-
-
-html.H5('Bubble Chart of Housing Prices by Legal Type'),
-dcc.Graph(id="bc_housing_price"),
-html.Br(),
-
-
-html.H5('Total Amount of houses in Different Neighborhood'),
-dcc.Graph(id="pc_housing_price"),      
-    
-
-])
 
 
 
@@ -111,7 +149,7 @@ def housing_price_histogram(year_slcted, neighborhood_slcted, type_property_slct
     xaxis_title_text='House Prices ($)', # xaxis label
     yaxis_title_text='Number of Houses', # yaxis label
     bargap=0.1)
-    
+
     bc = px.scatter(df, 
                     x="Geo Local Area",
                     y="current_land_value",
